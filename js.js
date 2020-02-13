814c7eae0c5bf82ba4c2d384d6cdd5339ee677b3
YuChen <yuchen@YuChendeiMac.local>
2020-02-14 02:33:49 +0800

    將Footer轉到外部檔案Footer.txt

--- js.js
+++ js.js
@@ -1,14 +1,25 @@
 if($(window).width()<768){
     $.get('Header_MO.txt',function(data){
         $('#Header').html(data)
-        $('Footer').removeClass('bg-dark')
+    })
+    $.get('Footer.txt',function(data){
+        $('#Footer').html(data)
+        $('Footer').removeClass('bg-dark','text-white')
         $('Footer').addClass('text-secondary')
-    })}else{
+    })
+}else{
     $.get('Header_DE.txt',function(data){
         Header=data
         $('#Header').html(Header)
         $('Footer').addClass('text-secondary')
 })
+$.get('Footer.txt',function(data){
+    $('#Footer').html(data)
+})
+
+
+
+
 $("#filter_btn").click(function(){
     $('#filter').toggleClass("filter_tgl");
 })}
