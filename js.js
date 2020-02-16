4cac4868e5059a0a20448d2573693c82ec5b9c14
0525033 <46869238+0525033@users.noreply.github.com>
2020-02-16 18:02:06 +0800

    把Footer移到外部檔案Footer.txt

--- js.js
+++ js.js
@@ -1,17 +1,21 @@
 if($(window).width()<768){
     $.get('Header_MO.txt',function(data){
         $('#Header').html(data)
-        $('Footer').removeClass('bg-dark')
+    })
+    $.get('Footer.txt',function(data){
+        $('#Footer').html(data)
+        $('Footer').removeClass('bg-dark','text-white')
         $('Footer').addClass('text-secondary')
-    })}else{
+    })
+}else{
     $.get('Header_DE.txt',function(data){
         Header=data
         $('#Header').html(Header)
         $('Footer').addClass('text-secondary')
-})
-$("#filter_btn").click(function(){
-    $('#filter').toggleClass("filter_tgl");
-})}
+})};
+$.get('Footer.txt',function(data){
+    $('#Footer').html(data)
+});
 
 if($(window).width()<768){
     $("#Item_content .container").addClass("container-fluid");
